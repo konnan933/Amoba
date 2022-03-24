16a9d4b6e69a3df0700d5f0bb1449f86c7ceef9c
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-03-24 22:36:35 +0100

    név adás nem jó de kinézet jó(picit javitani)

--- javascript.js
+++ javascript.js
@@ -10,10 +10,16 @@ function CLASS(elem) {
 function $(elem) {
     return document.querySelectorAll(elem);
 }
-
+var nev_1 =""
+var nev_2 =""
 var szimbolum = "X";
 var lepesek = 1;
 var lepeslista = [];
+
+
+
+
+
 function generalas(){
     var txt = "";
     for (let index = 0; index < 9; index++) {
@@ -26,16 +32,25 @@ function generalas(){
 }
 function init() {
     generalas();
+    nevadas();
 }
 function letetel(){
     leptetes();
     console.log(event.target.id);
     var kattintott = event.target.id;
-    nyerte();
+    //nyerte();
     lepeslista.push(kattintott);
     ID(kattintott).innerHTML="<p>"+szimbolum+"</p>"
     ID(kattintott).removeEventListener("click",letetel)
     ID(kattintott).style.background="lightgray";
+}
+function nevadas(){
+    document.getElementById("hajra").addEventListener("click", function () {
+        nev_1 = nev1Submit();
+        nev_2 = nev2Submit();
+      });
+    console.log(nev_1)
+    console.log(nev_2)
 }
 function leptetes(){  
     if (lepesek % 2 == 1){
@@ -46,7 +61,13 @@ function leptetes(){
         lepesek++;
     }
 }
-function nyerte(){
-    for (let index = 0; index < lepeslista.length; index++) {
-    }
+//function nyert_e(){
+//    for (let index = 0; index < lepeslista.length; index++) {
+//   }
+//}
+function nev1Submit(){
+    return document.getElementById("j1");
+}
+function nev2Submit(){
+    return document.getElementById("j2");
 }
